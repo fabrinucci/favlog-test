8dd233d4fd2480cd4c35a1921454cc5dea9dc7da
Fabrizio <fabrinucci99@gmail.com>
2024-02-22 15:18:04 -0300

    Add revalidate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import { Movies } from './types';
 
 const getMovies = async (): Promise<Movies> => {
   const res = await fetch(
-    'https://api.themoviedb.org/3/movie/popular?api_key=508a25f671b0a06c9ab8aec35944749e'
+    'https://api.themoviedb.org/3/movie/popular?api_key=508a25f671b0a06c9ab8aec35944749e',
+    { next: { revalidate: 3600 } }
   );
 
   return res.json();
